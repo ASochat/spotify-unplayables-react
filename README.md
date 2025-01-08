1e0ad263ea8c435ee8c501871f1e8aa0281e2eda
ASochat <antoine.sochat@gmail.com>
2025-01-08 15:51:31 +0100

    title, description, logo, README

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
-# React + Vite
+# Find your unplayable Spotify tracks at once. 
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Website running on spotify.soch.at.
 
-Currently, two official plugins are available:
+Built on React. 
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Any suggestions? File an issue or contact me directly at antoine@soch.at.
+
+///////// TO DO //////////
+
+- Pass the accessToken to the App instead of having a local storage (not a priority)
+
+- (not important yet) Try / Catch the access token to see if needs to be refreshed, with a refresh Token (or reconnect Data) but it's also a matter of checking whether we already have the userData (or if we want to make it dynamic, refresh at every reload which is probably best)
+
+- Change "Connect to Spotify" to "Refresh Data" if existing userData, using state
+
+- Remove ?code= after it is fetched (window.location just refreshed infinitely). That should probably go through a redirection.
+
+- Get the number of times it has been used, and save that to the server - should post a new "id" whenever it has been used. 
+
+- Loading circle: we don't have the number of tracks. There may be a way to get the first and last track, with a check a dichotomy, but for now we just use a dummy loading bar based on 3000 songs saved. 
+
+- Loading circle: improve design.
+
+- Add a comment interface below. 
+
+- sass compile in build mode: https://blog.logrocket.com/using-bootstrap-react-tutorial-examples/
+
+//////////////////////////
--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,3 @@
-/////////// TO FIX /////////////////////////////////////////////
-// - Post Authorization after the first connection, there's something that doesn't work, perhaps the Promise is not ready yet. I need to refresh for it to work. 
-////////////////////////////////////////////////////////////////
-
-/////////// TO DO /////////////////////////////////////////////
-// - App: when the userData is not defined (when you load the website for the first time bastically), it doesn't work
-// ...because it can't get the specific variable. There's probably something to do with useState and userEffect but I'm blocked for now
-//
-// - Pass the accessToken to the App instead of having a local storage (but need to have one for refreshes no ?)
-//
-// - Refresh the main render when userData is fetched (can't use create root, there's probably a refresh root method)
-// ...for now I put it in local storage so it seems like it works
-//
-// - (not important yet) Try / Catch the access token to see if needs to be refreshed, with a refresh Token (or reconnect Data)
-// ...but it's also a matter of checking whether we already have the userData (or if we want to make it dynamic, refresh at every
-// ...reload which is probably best)
-//
-// - Change Connect to Spotify to "Refresh Data" if existing userData, using state
-//
-// - Fetching all songs takes time. We can use offset to do a loader for the user to wait
-// ...yet the app won't render until it's ready. Should use a data background refresher instead.
-//
-// - Remove ?code= after it is fetched (window.location just refreshed infinitely)
-//
-// - Use effect-hooks (use effect) instead of main to connect to Spotify. And use JSON server to store the data instead of localStorage? Yes but it's not the same data for everyone
-//
-// - Get the number of times it has been used, and save that to the server
-//
-// - Loading circle: we don't have the number of tracks. There may be a way to get the first and last track, with a check a dichotomy, but 
-// for now we just use a dummy loading bar based on 3000 songs saved. 
-//
-// - Add a comment interface
-////////////////////////////////////////////////////////////////
-
-
 console.log('STARTING MAIN.JSX')
 
 // Import main modules
